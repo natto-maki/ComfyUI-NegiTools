678f137f3d48e7f95e2d78418a0e8d9f90787c3b
NattoMaki <negi.in.natto.maki@gmail.com>
2023-11-20 22:43:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# ComfyUI-NegiTools
+
+## Installation
+
+- Install dependencies: pip install openai
+
+- Clone the repository: git clone https://github.com/natto-maki/ComfyUI-NegiTools.git
+  to your ComfyUI custom_nodes directory
+
+  The script will then automatically install all custom scripts and nodes.
+  It will attempt to use symlinks and junctions to prevent having to copy files and keep them up to date.
+
+- To launch: Set the OPENAI_API_KEY environment variable before launching ComfyUI. 
+
+  e.g., OPENAI_API_KEY=sk-<...your API key here...> python3 main.py ...
+
+- For uninstallation:
+  Delete the cloned repo in custom_nodes
+
+## Update
+
+1. Navigate to the cloned repo e.g. custom_nodes/ComfyUI-NegiTools
+2. git pull
+
---
+++ openai_dalle3.py
@@ -0,0 +1,60 @@
+import openai
+import base64
+import io
+
+import torch
+from PIL import Image
+from torchvision.transforms import functional as TF
+
+
+class OpenAiDalle3:
+    def __init__(self):
+        self.__client = openai.OpenAI()
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "resolution": (["1024x1024", "1024x1792", "1792x1024"],),
+                "dummy_seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                "prompt": ("STRING", {
+                    "multiline": True,
+                    "default": "great picture"
+                })
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE", "INT", "INT")
+    RETURN_NAMES = ("IMAGE", "WIDTH", "HEIGHT")
+    FUNCTION = "doit"
+    OUTPUT_NODE = True
+    CATEGORY = "Generator"
+
+    def doit(self, resolution, dummy_seed, prompt):
+        _ = dummy_seed
+        r0 = self.__client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size=resolution,
+            quality="hd",  # "standard"
+            n=1,
+            response_format="b64_json"
+        )
+        im0 = Image.open(io.BytesIO(base64.b64decode(r0.data[0].b64_json)))
+        im1 = TF.to_tensor(im0.convert("RGBA"))
+        im1[:3, im1[3, :, :] == 0] = 0
+
+        images = torch.stack([im1])
+        images = images.permute(0, 2, 3, 1)
+        images = images[:, :, :, :3]
+        widths = resolution.split("x")
+        return images, widths[0], widths[1]
+
+
+NODE_CLASS_MAPPINGS = {
+    "OpenAiDalle3": OpenAiDalle3
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "OpenAiDalle3": "OpenAI DALLe3"
+}
